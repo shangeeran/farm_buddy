c096e65302eab56674b94f6d157a37a849e0f342
zansangeeth <42418189+zansangeeth@users.noreply.github.com>
2020-03-19 16:31:30 +0530

    removed files

---
+++ play.js
@@ -0,0 +1,10 @@
+var name = 'max';
+var age  = 24;
+var hasHobbies = true;
+
+function userDetails(userName, userAge, userHobbies){
+
+    return ('name is ' + userName + ' age is ' + userAge + ' have hobbies ' + userHobbies);
+}
+
+console.log(userDetails(name,age,hasHobbies));
