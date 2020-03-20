4eb91f8dff791bbfbcb75c732237edf62b028b29
zansangeeth <42418189+zansangeeth@users.noreply.github.com>
2020-03-20 22:20:53 +0530

    change function

--- play.js
+++ play.js
@@ -2,11 +2,7 @@ var name = 'max';
 var age  = 24;
 var hasHobbies = true;
 
-var post  = 'sangeeth';
-var col = 'haniman';
-var 
-
-function userDetails(userName, userAge, userHobbies){
+const userDetails = (userName, userAge, userHobbies) => {
 
     return ('name is ' + userName + ' age is ' + userAge + ' have hobbies ' + userHobbies);
 }
