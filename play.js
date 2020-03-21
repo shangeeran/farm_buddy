6415ade89d285d1bf4f6cc1a76122cb2561491f7
zansangeeth <42418189+zansangeeth@users.noreply.github.com>
2020-03-21 19:35:41 +0530

    differrent types of funtioanalities

--- play.js
+++ play.js
@@ -7,4 +7,17 @@ const userDetails = (userName, userAge, userHobbies) => {
     return ('name is ' + userName + ' age is ' + userAge + ' have hobbies ' + userHobbies);
 }
 
+const sum = (a,b) =>{
+    return a+b;
+}
+
+const sum1 = (a,b) => a+b;
+const sumOne = a => a+5;
+const addR = () => 8+6;
+
+console.log(addR());
+console.log(sumOne(15));
+console.log(sum1(5,6));
+console.log(sum(1,2));
+
 console.log(userDetails(name,age,hasHobbies));
