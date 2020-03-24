7087b97208929f547521fc207dd233766917463d
zansangeeth <42418189+zansangeeth@users.noreply.github.com>
2020-03-24 20:00:41 +0530

    async methods

---
+++ async.js
@@ -0,0 +1,16 @@
+// setTimeout(() => {
+//     console.log("time out!!")
+// },3000)
+
+let timeout = () =>{
+    setTimeout(() => {
+        console.log("time out!!");
+    },1)
+    
+}
+
+timeout();
+
+console.log("hi");
+console.log("hi");
+console.log("hi");
