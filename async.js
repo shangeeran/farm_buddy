945664ed0ba9e2a93f0f3d7dbaafebbbc2b7fd9e
zansangeeth <42418189+zansangeeth@users.noreply.github.com>
2020-03-24 20:48:02 +0530

    async time out funtions with resolve parameter

--- async.js
+++ async.js
@@ -9,17 +9,25 @@
     
 // }
 
-const fetchData = (callback) =>{
-    setTimeout(() =>{
-        callback("done!!")
-    },1500)
-}
+const fetchData = () => {
+    const promise = new Promise((resolve,reject) => {
+        setTimeout(() =>{
+            resolve("done!!")
+        },1500)
+        
+    });
+    return promise;
+    
+};
 
 setTimeout(() => {
     console.log("timer is done!");
-    fetchData((text) =>{
+    fetchData().then(text => {
         console.log(text);
-    })
+        return fetchData().then(text2 =>{
+            console.log(text2);
+        })
+    });
 },2000)
 
 // timeout();
