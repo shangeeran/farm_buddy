38ec02a11e460c9a31cb3f3bbb38ecd59c3b27cf
zansangeeth <42418189+zansangeeth@users.noreply.github.com>
2020-03-24 20:23:34 +0530

    callback functions

--- async.js
+++ async.js
@@ -2,14 +2,27 @@
 //     console.log("time out!!")
 // },3000)
 
-let timeout = () =>{
-    setTimeout(() => {
-        console.log("time out!!");
-    },1)
+// let timeout = () =>{
+//     setTimeout(() => {
+//         console.log("time out!!");
+//     },3000)
     
+// }
+
+const fetchData = (callback) =>{
+    setTimeout(() =>{
+        callback("done!!")
+    },1500)
 }
 
-timeout();
+setTimeout(() => {
+    console.log("timer is done!");
+    fetchData((text) =>{
+        console.log(text);
+    })
+},2000)
+
+// timeout();
 
 console.log("hi");
 console.log("hi");
