88195bde7aa46fe8efc54644c3ab1c0f1d031cbc
shangeeran <shangeeran@gmail.com>
2020-04-12 03:32:58 +0000

    form format change

--- screens/proSetting.js
+++ screens/proSetting.js
@@ -1,68 +1,82 @@
 import React, {Component} from 'react';
-import {StyleSheet, Button, View, Text, Image, TextInput, ScrollView} from 'react-native';
+import {StyleSheet, Button, View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scrollview'
 
-export default class SignUp extends React.Component {
-    state = {
-        username: '',
-        password: '',
-        email: '',
-        phone_number: '',
-    };
-    onChangeText = (key, val) => {
-        this.setState({[key]: val});
-    };
+export default class proSetting extends React.Component {
+    // state = {
+    //     username: '',
+    //     password: '',
+    //     email: '',
+    //     phone_number: '',
+    // };
+    // onChangeText = (key, val) => {
+    //     this.setState({[key]: val});
+    // };
 
     render() {
         return (
-            <View style={styles.container}>
-                <View style={styles.image1}>
-                    <Image source={require('./assets/farmer.png')} style={{width: 150, height: 150}} />
-                </View>
+            <KeyboardAwareScrollView
+                keyboardShouldPersistTaps="handled"
+                resetScrollToCoords={{ x: 0, y: 0 }}
+                keyboardOpeningTime={0}
+                scrollEnabled={true}
+            >
+                <View style={styles.container}>
+                    <View style={styles.proImg}>
+                        <Image source={require('./assets/farmer.png')} style={{width: 150, height: 150}} />
+                    </View>
 
-                <View>
-                    <Text>First Name</Text>
-                </View>
-                <TextInput
-                    style={styles.input}
-                    autoCapitalize="none"
-                    onChangeText={val => this.onChangeText('fName', val)}
-                />
-                <View>
-                    <Text>Last Name</Text>
-                </View>
-                <TextInput
-                    style={styles.input}
-                    autoCapitalize="none"
-                    onChangeText={val => this.onChangeText('lName', val)}
-                />
-                <View>
-                    <Text>NIC</Text>
-                </View>
-                <TextInput
-                    style={styles.input}
-                    autoCapitalize="none"
-                    onChangeText={val => this.onChangeText('nic', val)}
-                />
-                <View>
-                    <Text>Location</Text>
-                </View>
-                <TextInput
-                    style={styles.input}
-                    autoCapitalize="none"
-                    onChangeText={val => this.onChangeText('location', val)}
-                />
-                <View>
-                    <Text>Soil Type</Text>
-                </View>
-                <TextInput
-                    style={styles.input}
-                    autoCapitalize="none"
-                    onChangeText={val => this.onChangeText('soilType', val)}
-                />
+                    <View style={styles.textVisible}>
+                        <Text>First Name</Text>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        onChangeText={val => this.onChangeText('fName', val)}
+                    />
+                    <View style={styles.textVisible}>
+                        <Text>Last Name</Text>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        onChangeText={val => this.onChangeText('lName', val)}
+                    />
+                    <View style={styles.textVisible}>
+                        <Text>NIC No</Text>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        onChangeText={val => this.onChangeText('nic', val)}
+                    />
+                    <View style={styles.textVisible}>
+                        <Text>Location</Text>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        onChangeText={val => this.onChangeText('location', val)}
+                    />
+                    <View style={styles.textVisible}>
+                        <Text>Soil Type</Text>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        onChangeText={val => this.onChangeText('soilType', val)}
+                    />
 
 
-                <Button style={{bottom: 0}} title="Next" onPress={this.next} />
-            </View>
+                    <TouchableOpacity style = {styles. buttonContainer}
+                                      onPress={() => this.props.navigation.navigate('xxxx', { screenName: "xxxx" })}
+                    >
+                        <Text style = {styles.textNext}>
+                            Next
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAwareScrollView>
         );
     }
 }
@@ -72,17 +86,40 @@ const styles = StyleSheet.create({
         width: 350,
         height: 40,
         backgroundColor: '#FFFFFF',
-        margin: 20,
+        marginTop: 5,
+        marginBottom: 15,
         padding: 8,
         color: 'black',
         borderRadius: 14,
         fontSize: 18,
-        // fontWeight: '500',
+        fontWeight: 'bold',
     },
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    proImg: {
+        padding: '5%'
+    },
+    textVisible: {
+        paddingRight: '60%',
+        fontSize: 18,
+    },
+    buttonContainer : {
+        bottom:0,
+        borderWidth: 1,
+        borderRadius: 30,
+        paddingVertical: 10,
+        paddingHorizontal: 30,
+        borderColor: '#05C167',
+        backgroundColor: '#05C167',
+    },
+    textNext: {
+        fontFamily: 'serif',
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff',
     },
 });
 
