45c78f0cab76e92d9586fa90dcaf530a5425aa2a
shangeeran <shangeeran@gmail.com>
2020-04-12 02:51:25 +0000

    form change

--- screens/proSetting.js
+++ screens/proSetting.js
@@ -1,34 +1,6 @@
 import React, {Component} from 'react';
 import {StyleSheet, Button, View, Text, Image, TextInput, ScrollView} from 'react-native';
 
-
-// export default class App extends Component {
-//     render() {
-//         return (
-//             <View style={styles.image1}>
-//                 <Image source={require('./assets/farmer.png')} style={{width: 150, height: 150}} />
-//             </View>
-//         );
-//     }
-// }
-//
-// const styles = StyleSheet.create({
-//     container : {
-//         flex: 1,
-//         alignItems: 'center',
-//         // justifyContent: 'center',
-//     },
-//     image1: {
-//         marginTop: '5%',
-//         flexGrow:1,
-//         height:null,
-//         width:null,
-//         alignItems: 'center',
-//         // justifyContent:'center',
-//     },
-//
-// });
-
 export default class SignUp extends React.Component {
     state = {
         username: '',
@@ -39,15 +11,6 @@ export default class SignUp extends React.Component {
     onChangeText = (key, val) => {
         this.setState({[key]: val});
     };
-    // signUp = async () => {
-    //   //   const {username, password, email, phone_number} = this.state;
-    //   //   try {
-    //   //     // here place your signup logic
-    //   //     console.log('user successfully signed up!: ', success);
-    //   //   } catch (err) {
-    //   //     console.log('error signing up: ', err);
-    //   //   }
-    //   // };
 
     render() {
         return (
@@ -55,37 +18,50 @@ export default class SignUp extends React.Component {
                 <View style={styles.image1}>
                     <Image source={require('./assets/farmer.png')} style={{width: 150, height: 150}} />
                 </View>
+
+                <View>
+                    <Text>First Name</Text>
+                </View>
                 <TextInput
                     style={styles.input}
-                    placeholder="Username"
                     autoCapitalize="none"
-                    placeholderTextColor="black"
-                    onChangeText={val => this.onChangeText('username', val)}
+                    onChangeText={val => this.onChangeText('fName', val)}
                 />
+                <View>
+                    <Text>Last Name</Text>
+                </View>
                 <TextInput
                     style={styles.input}
-                    placeholder="Password"
-                    secureTextEntry={true}
                     autoCapitalize="none"
-                    placeholderTextColor="black"
-                    onChangeText={val => this.onChangeText('password', val)}
+                    onChangeText={val => this.onChangeText('lName', val)}
                 />
+                <View>
+                    <Text>NIC</Text>
+                </View>
                 <TextInput
                     style={styles.input}
-                    placeholder="Email"
                     autoCapitalize="none"
-                    placeholderTextColor="white"
-                    onChangeText={val => this.onChangeText('email', val)}
+                    onChangeText={val => this.onChangeText('nic', val)}
                 />
+                <View>
+                    <Text>Location</Text>
+                </View>
                 <TextInput
                     style={styles.input}
-                    placeholder="Phone Number"
                     autoCapitalize="none"
-                    placeholderTextColor="black"
-                    onChangeText={val => this.onChangeText('phone_number', val)}
+                    onChangeText={val => this.onChangeText('location', val)}
+                />
+                <View>
+                    <Text>Soil Type</Text>
+                </View>
+                <TextInput
+                    style={styles.input}
+                    autoCapitalize="none"
+                    onChangeText={val => this.onChangeText('soilType', val)}
                 />
 
-                <Button style={{bottom: 0}} title="Sign Up" onPress={this.signUp} />
+
+                <Button style={{bottom: 0}} title="Next" onPress={this.next} />
             </View>
         );
     }
@@ -94,14 +70,14 @@ export default class SignUp extends React.Component {
 const styles = StyleSheet.create({
     input: {
         width: 350,
-        height: 55,
+        height: 40,
         backgroundColor: '#FFFFFF',
         margin: 20,
         padding: 8,
         color: 'black',
         borderRadius: 14,
         fontSize: 18,
-        fontWeight: '500',
+        // fontWeight: '500',
     },
     container: {
         flex: 1,
