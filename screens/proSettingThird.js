be4939eda2cb5fea44bdc7b71c06bd48fc12bd85
shangeeran <shangeeran@gmail.com>
2020-04-12 23:52:39 +0000

    update button

--- screens/proSettingThird.js
+++ screens/proSettingThird.js
@@ -16,7 +16,7 @@ export default class proSettingThird extends React.Component {
                         <Image source={require('./assets/farmer.png')} style={{width: 150, height: 150}} />
                     </View>
 
-                    <View>
+                    <View style={styles.text}>
                         <Text>Language</Text>
                     </View>
                     <TouchableOpacity style = {styles.lanButton}
@@ -26,11 +26,11 @@ export default class proSettingThird extends React.Component {
                         </Text>
                     </TouchableOpacity>
 
-                    <View>
+                    <View style={styles.text}>
                         <Text>Push Notification</Text>
                     </View>
 
-                    <View>
+                    <View style={styles.text}>
                         <Text>SMS Notification</Text>
                     </View>
 
@@ -60,6 +60,8 @@ const styles = StyleSheet.create({
     },
     buttonContainer : {
         bottom:0,
+        marginTop:120,
+        marginBottom: 40,
         borderWidth: 1,
         borderRadius: 30,
         paddingVertical: 10,
@@ -68,13 +70,15 @@ const styles = StyleSheet.create({
         backgroundColor: '#05C167',
     },
     lanButton: {
-        bottom:0,
+        // bottom:0,
         borderWidth: 1,
         borderRadius: 30,
-        paddingVertical: 10,
-        paddingHorizontal: 30,
+        paddingVertical: 5,
+        paddingHorizontal: 25,
         borderColor: '#05C167',
-        backgroundColor: '#05C167'
+        backgroundColor: '#05C167',
+        marginLeft: 250,
+        marginTop: -40
     },
     textNext: {
         fontFamily: 'serif',
@@ -82,6 +86,11 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#fff',
     },
+    text: {
+        marginLeft: -250,
+        marginTop:50,
+        marginBottom:20,
+    }
 
 });
 
