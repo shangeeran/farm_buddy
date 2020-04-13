6201cee178ad3de6e0552874fbb53f2032234d99
shangeeran <shangeeran@gmail.com>
2020-04-13 01:24:56 +0000

    visible messages add

--- screens/proSettingEnd.js
+++ screens/proSettingEnd.js
@@ -10,6 +10,9 @@ export default class proSettingThird extends React.Component {
                 <View style={styles.proImg}>
                     <Image source={require('./assets/successful.jpg')} style={{width: 300, height: 300}} />
                 </View>
+                <View>
+                    <Text style={styles.text}>Successfully Changed!</Text>
+                </View>
 
             </View>
         );
@@ -25,6 +28,13 @@ const styles = StyleSheet.create({
     },
     proImg: {
         padding: '5%'
+    },
+    text: {
+        marginTop:10,
+        fontFamily: 'serif',
+        fontSize: 40,
+        fontWeight: 'bold',
+        textAlign: 'center'
     }
 });
 
