6b73e3de4e7e42275d37cf5eb0e927d8ef4314cc
shangeeran <shangeeran@gmail.com>
2020-04-14 00:31:46 +0000

    onpress add

--- screens/proSetting.js
+++ screens/proSetting.js
@@ -26,52 +26,50 @@ export default class proSetting extends React.Component {
                         <Image source={require('./assets/farmer.png')} style={{width: 150, height: 150}} />
                     </View>
 
-                    <View style={styles.bottomContainer}>
-                        <View style={styles.textVisible}>
-                            <Text>First Name</Text>
-                        </View>
-                        <TextInput
-                            style={styles.input}
-                            autoCapitalize="none"
-                            onChangeText={val => this.onChangeText('fName', val)}
-                        />
-                        <View style={styles.textVisible}>
-                            <Text>Last Name</Text>
-                        </View>
-                        <TextInput
-                            style={styles.input}
-                            autoCapitalize="none"
-                            onChangeText={val => this.onChangeText('lName', val)}
-                        />
-                        <View style={styles.textVisible}>
-                            <Text>NIC No</Text>
-                        </View>
-                        <TextInput
-                            style={styles.input}
-                            autoCapitalize="none"
-                            onChangeText={val => this.onChangeText('nic', val)}
-                        />
-                        <View style={styles.textVisible}>
-                            <Text>Location</Text>
-                        </View>
-                        <TextInput
-                            style={styles.input}
-                            autoCapitalize="none"
-                            onChangeText={val => this.onChangeText('location', val)}
-                        />
-                        <View style={styles.textVisible}>
-                            <Text>Soil Type</Text>
-                        </View>
-                        <TextInput
-                            style={styles.input}
-                            autoCapitalize="none"
-                            onChangeText={val => this.onChangeText('soilType', val)}
-                        />
+                    <View style={styles.textVisible}>
+                        <Text>First Name</Text>
                     </View>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        onChangeText={val => this.onChangeText('fName', val)}
+                    />
+                    <View style={styles.textVisible}>
+                        <Text>Last Name</Text>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        onChangeText={val => this.onChangeText('lName', val)}
+                    />
+                    <View style={styles.textVisible}>
+                        <Text>NIC No</Text>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        onChangeText={val => this.onChangeText('nic', val)}
+                    />
+                    <View style={styles.textVisible}>
+                        <Text>Location</Text>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        onChangeText={val => this.onChangeText('location', val)}
+                    />
+                    <View style={styles.textVisible}>
+                        <Text>Soil Type</Text>
+                    </View>
+                    <TextInput
+                        style={styles.input}
+                        autoCapitalize="none"
+                        onChangeText={val => this.onChangeText('soilType', val)}
+                    />
 
 
                     <TouchableOpacity style = {styles. buttonContainer}
-                                      onPress={() => this.props.navigation.navigate('xxxx', { screenName: "xxxx" })}
+                                      onPress={() => this.props.navigation.navigate('proSettingSec', { screenName: "proSettingSec" })}
                     >
                         <Text style = {styles.textNext}>
                             Next
@@ -84,33 +82,22 @@ export default class proSetting extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#FFFFFF'
-    },
-    bottomContainer: {
-        // flex: 1,
-        borderRadius: 20,
-        marginBottom: 10,
-        backgroundColor: '#EFEFEF',
-        height: '62%',
-        width: '90%'
-        // overflow: 'hidden',
-    },
     input: {
         width: 350,
         height: 40,
         backgroundColor: '#FFFFFF',
         marginTop: 5,
         marginBottom: 15,
-        marginLeft:10,
         padding: 8,
         color: 'black',
         borderRadius: 14,
         fontSize: 18,
         fontWeight: 'bold',
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     proImg: {
         padding: '5%'
@@ -118,8 +105,6 @@ const styles = StyleSheet.create({
     textVisible: {
         paddingRight: '60%',
         fontSize: 18,
-        marginLeft:10,
-        marginTop: 5
     },
     buttonContainer : {
         bottom:0,
