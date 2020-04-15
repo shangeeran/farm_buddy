d0a0216d7d79e2332c65fa88c3131ed6c5d6ad71
unknown <shangeeran@gmail.com>
2020-04-15 11:41:51 +0000

    img add

--- screens/OnBoard.js
+++ screens/OnBoard.js
@@ -1,4 +1,4 @@
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 import React from 'react';
 
 import Onboarding from 'react-native-onboarding-swiper';
@@ -9,19 +9,19 @@ const Simple = () => (
         pages={[
             {
                 backgroundColor: '#fff',
-                image: <Image source={require('./images/circle.png')} />,
+                image: <Image source={require('./assets/pic1.png')} style={{width: 150, height: 150}} />,
                 title: 'Onboarding',
                 subtitle: 'Done with React Native Onboarding Swiper',
             },
             {
                 backgroundColor: '#fe6e58',
-                image: <Image source={require('./images/square.png')} />,
+                image: <Image source={require('./assets/pic2.png')} style={{width: 150, height: 150}} />,
                 title: 'The Title',
                 subtitle: 'This is the subtitle that sumplements the title.',
             },
             {
                 backgroundColor: '#999',
-                image: <Image source={require('./images/triangle.png')} />,
+                image: <Image source={require('./assets/pic3.png')} style={{width: 150, height: 150}} />,
                 title: 'Triangle',
                 subtitle: "Beautiful, isn't it?",
             },
