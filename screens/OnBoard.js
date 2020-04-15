c12cdd9c600c8259fc3996914d2b4e51f8275b75
unknown <shangeeran@gmail.com>
2020-04-15 11:35:48 +0000

    test

--- screens/OnBoard.js
+++ screens/OnBoard.js
@@ -1,55 +1,32 @@
-import 'react-native-gesture-handler';
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, Image} from 'react-native';
-import AppIntroSlider from 'react-native-app-intro-slider';
 
-const styles = StyleSheet.create({
-    img: {},
-    tit: {
-        alignItems: 'center',
-        justifyContent: 'space-around',
-    },
-});
-
-const slides = [
-    {
-        key: 'onBoard1',
-        image: require('../../assets/img/pic1.png'),
-        title: 'No More Improper Knowledge',
-        backgroundColor: '#fff',
-    },
-    {
-        key: 'onBoard2',
-        image: require('../../assets/img/pic2.png'),
-        title: 'No More Harvest Wastage',
-        backgroundColor: '#fff',
-    },
-    {
-        key: 'onBoard3',
-        image: require('../../assets/img/pic3.png'),
-        title: 'Your Efforts Will Pay Off',
-        backgroundColor: '#fff',
-    },
-];
-
-export default class OnBoard extends Component {
-    _renderItem = ({item}) => {
-        return (
-            <View>
-                <Image source={item.image} />
-                <Text style={styles.tit}>{item.title}</Text>
-            </View>
-        );
-    };
-
+class ShanOnboard extends Component {
     render() {
         return (
-            <AppIntroSlider
-                slides={slides}
-                renderItem={this._renderItem}
-                bottomButton
-                showSkipButton
+            <Onboarding
+                pages={[
+                    {
+                        backgroundColor: '#fff',
+                        image: <Image source={require('../assets/pic1.png')} />,
+                        title: 'Onboarding',
+                        subtitle: 'Done with React Native Onboarding Swiper',
+                    },
+                    {
+                        backgroundColor: '#fff',
+                        image: <Image source={require('./images/circle.png')} />,
+                        title: 'Onboarding',
+                        subtitle: 'Done with React Native Onboarding Swiper',
+                    },
+                    {
+                        backgroundColor: '#fff',
+                        image: <Image source={require('./images/circle.png')} />,
+                        title: 'Onboarding',
+                        subtitle: 'Done with React Native Onboarding Swiper',
+                    },
+                ]}
             />
         );
     }
 }
+
+export default ShanOnboard;
