02d2c1c181c04f9cdec717c1d7a53de1c917c022
unknown <shangeeran@gmail.com>
2020-04-15 11:40:10 +0000

    formet change

--- screens/OnBoard.js
+++ screens/OnBoard.js
@@ -1,32 +1,32 @@
-import React, {Component} from 'react';
+import { Image } from 'react-native';
+import React from 'react';
 
-class ShanOnboard extends Component {
-    render() {
-        return (
-            <Onboarding
-                pages={[
-                    {
-                        backgroundColor: '#fff',
-                        image: <Image source={require('../assets/pic1.png')} />,
-                        title: 'Onboarding',
-                        subtitle: 'Done with React Native Onboarding Swiper',
-                    },
-                    {
-                        backgroundColor: '#fff',
-                        image: <Image source={require('./images/circle.png')} />,
-                        title: 'Onboarding',
-                        subtitle: 'Done with React Native Onboarding Swiper',
-                    },
-                    {
-                        backgroundColor: '#fff',
-                        image: <Image source={require('./images/circle.png')} />,
-                        title: 'Onboarding',
-                        subtitle: 'Done with React Native Onboarding Swiper',
-                    },
-                ]}
-            />
-        );
-    }
-}
+import Onboarding from 'react-native-onboarding-swiper';
 
-export default ShanOnboard;
+const Simple = () => (
+    <Onboarding
+        onDone={() => console.log('done')}
+        pages={[
+            {
+                backgroundColor: '#fff',
+                image: <Image source={require('./images/circle.png')} />,
+                title: 'Onboarding',
+                subtitle: 'Done with React Native Onboarding Swiper',
+            },
+            {
+                backgroundColor: '#fe6e58',
+                image: <Image source={require('./images/square.png')} />,
+                title: 'The Title',
+                subtitle: 'This is the subtitle that sumplements the title.',
+            },
+            {
+                backgroundColor: '#999',
+                image: <Image source={require('./images/triangle.png')} />,
+                title: 'Triangle',
+                subtitle: "Beautiful, isn't it?",
+            },
+        ]}
+    />
+);
+
+export default Simple;
