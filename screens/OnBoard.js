4950fe4b5c8f5b9b6e2c8d8ebc0e3468d3d4b658
ShangarShanan <shangar.shan33@gmail.com>
2020-04-16 12:02:18 +0530

    OnboardDone

--- screens/OnBoard.js
+++ screens/OnBoard.js
@@ -1,29 +1,58 @@
 import React from 'react';
-import { Image, View } from 'react-native';
+import {Image, Text, StyleSheet} from 'react-native';
 
 import Onboarding from 'react-native-onboarding-swiper';
 
-const OnBoard = () => (
+const App = () => (
     <Onboarding
         onDone={() => console.log('done')}
         pages={[
             {
                 backgroundColor: '#ffffff',
-                image: <Image source={require('./assets/pic1.png')} style={{width: '100%', height: '100%'}} />,
+                image: (
+                    <Image source={require('../assets/pic1.png')} style={styles.img1} />
+                ),
                 title: 'No more Improper Knowledge',
+                subtitle: '\n',
             },
             {
                 backgroundColor: '#ffffff',
-                image: <Image source={require('./assets/pic2.png')} style={{width: '100%', height: '100%'}} />,
+                image: (
+                    <Image source={require('../assets/pic2.png')} style={styles.img2} />
+                ),
                 title: 'No more Harvest Wastage',
+                subtitle: '\n',
             },
             {
                 backgroundColor: '#ffffff',
-                image: <Image source={require('./assets/pic3.png')} style={{width: '100%', height: '100%'}} />,
+                image: (
+                    <Image source={require('../assets/pic3.png')} style={styles.img3} />
+                ),
                 title: 'Your Efforts Will Pay Off',
             },
         ]}
     />
 );
 
-export default OnBoard;
+const styles = StyleSheet.create({
+    img1: {
+        height: 400,
+        width: 400,
+        marginLeft: 250,
+        marginBottom: 0,
+    },
+
+    img2: {
+        height: 400,
+        width: 400,
+    },
+
+    img3: {
+        height: 350,
+        width: 430,
+        marginRight: 100,
+        marginLeft: 50,
+    },
+});
+
+export default App;
