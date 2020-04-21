6fb104cae8202e65d9f95d94d590d1bd30406b33
shamry <48283240+shamry@users.noreply.github.com>
2020-04-21 21:09:47 +0530

    List of Crops page Header And Alignment Changed

--- screens/FlatListCrops.js
+++ screens/FlatListCrops.js
@@ -21,25 +21,41 @@ export default class Example extends Component {
     //Custom Layout for  each row of the FlatList
     renderItem = ({item}) => {
         return (
-            <View style={{flex: 1, flexDirection: 'row', marginBottom: 30, justifyContent: 'center'}}>
-    <Image
-        style={{width: 80, height: 80, margin: 5, borderRadius: 50}}
-        source={{uri: item.image}}
-        />
-        <View style={{flex: 1, justifyContent: 'center', marginLeft: 5}}>
-    <Text style={{fontSize: 18, color: 'green', marginBottom: 5, marginTop:20}}>
-        {item.book_title}
-    </Text>
-        <Text style={{fontSize: 13, color: 'black', marginBottom: 20}}>{item.author}</Text>
-        </View>
-        </View>
-    );
+            <View>
+                <View
+                    style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        marginBottom: 30,
+                        justifyContent: 'center',
+                    }}>
+                    <Image
+                        style={{width: 80, height: 80, margin: 5, borderRadius: 50}}
+                        source={{uri: item.image}}
+                    />
+                    <View style={{flex: 1, justifyContent: 'center', marginLeft: 5}}>
+                        <Text
+                            style={{
+                                fontSize: 18,
+                                color: 'green',
+                                marginBottom: 5,
+                                marginTop: 20,
+                            }}>
+                            {item.book_title}
+                        </Text>
+                        <Text style={{fontSize: 13, color: 'black', marginBottom: 20}}>
+                            {item.author}
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        );
     };
 
     renderSeparator = () => {
         return (
             <View style={{height: 1, width: '100%', backgroundColor: 'white'}} />
-    );
+        );
     };
 
     //Fetching Data from the server
@@ -50,7 +66,7 @@ export default class Example extends Component {
             .then(response => response.json())
             .then(responseJson => {
                 this.setState({
-                    dataSource: responseJson.book_array
+                    dataSource: responseJson.book_array,
                 });
             })
             .catch(error => {
@@ -59,16 +75,28 @@ export default class Example extends Component {
     }
 
     render() {
-        return(
+        return (
             <View style={styles.container}>
-            <FlatList
-        data={this.state.dataSource}
-        renderItem={this.renderItem}
-        keyExtractor={(item, index) => index}
-        ItemSeparatorComponent={this.renderSeparator}
-        />
-        </View>
-    );
+                <Text
+                    style={{
+                        fontSize: 18,
+                        left: 140,
+                        top: 20,
+                        fontWeight: 'bold',
+                        marginBottom: 30,
+                    }}>
+                    {' '}
+                    List of Crops{' '}
+                </Text>
+                <FlatList
+                    style={{margin: 20}}
+                    data={this.state.dataSource}
+                    renderItem={this.renderItem}
+                    keyExtractor={(item, index) => index}
+                    ItemSeparatorComponent={this.renderSeparator}
+                />
+            </View>
+        );
     }
 }
 
