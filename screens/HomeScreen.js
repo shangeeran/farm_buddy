09cff5ad9923fffc991a14309609cd99aac6256b
shamry <48283240+shamry@users.noreply.github.com>
2020-04-22 16:32:07 +0530

    Home Screen with 3 buttons Added

---
+++ screens/HomeScreen.js
@@ -0,0 +1,104 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableHighlight,
+    Alert,
+} from 'react-native';
+
+export default class SignUpView extends Component {
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {
+    //     district: '',
+    //   };
+    // }
+
+    onClickListener = viewId => {
+        Alert.alert('Alert', 'Button pressed ');
+    };
+
+    render() {
+        return (
+            <View>
+                <Text style={{top: 80, left: 80, fontSize: 20}}>
+                    Welcome to Farm Buddy
+                </Text>
+                <TouchableHighlight
+                    style={[styles.buttonContainer, styles.signupButton]}
+                    onPress={() => this.onClickListener()}>
+                    <Text style={styles.signUpText}>List of Crops</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight
+                    style={[styles.secondButton, styles.signupButton]}
+                    onPress={() => this.onClickListener()}>
+                    <Text style={styles.signUpText}>Dashboard</Text>
+                </TouchableHighlight>
+
+                <TouchableHighlight
+                    style={[styles.thirdButton, styles.signupButton]}
+                    onPress={() => this.onClickListener()}>
+                    <Text style={styles.signUpText}>Profile</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        top: 200,
+        height: 80,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        width: 380,
+        borderRadius: 5,
+        left: 15,
+        borderColor: 'black',
+    },
+    secondButton: {
+        top: 500,
+        height: 80,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        width: 380,
+        borderRadius: 5,
+        left: 15,
+        borderColor: 'black',
+    },
+    thirdButton: {
+        top: 500,
+        height: 80,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20,
+        width: 380,
+        borderRadius: 5,
+        left: 15,
+        borderColor: 'black',
+    },
+    signupButton: {
+        borderColor: 'black',
+        borderBottomColor: 'black',
+        backgroundColor: '#59D8A3',
+        top: 180,
+    },
+    signUpText: {
+        color: 'white',
+        fontSize: 30,
+    },
+    textStyle: {
+        fontSize: 18,
+        textAlign: 'left',
+        bottom: -50,
+        right: 0,
+    },
+});
