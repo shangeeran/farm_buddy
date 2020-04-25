8cc4f602f8811d5a7a3ecb7618fe85c667723991
unknown <shangeeran@gmail.com>
2020-04-25 17:56:21 +0000

    weather API add

--- screens/Weather.js
+++ screens/Weather.js
@@ -1,46 +1,75 @@
 import React, {Component} from 'react';
 import {View, StyleSheet, Text, Button, Image} from 'react-native';
-import { Text, Card, Divider } from 'react-native-elements';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+import axios from 'axios';
+import moment from "moment";
 
 export default class Weather extends Component {
+  state = {
+    data:{
+      main:{
+      },
+      weather:[],
+      wind:{}
+    },
+    currentDate: new Date(),
+    markedDate: moment(new Date()).format("MMMM Do YYYY") // MMMM Do YYYY, h:mm:ss a
+
+  }
+  async componentDidMount(){
+    const res = await axios.get('https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139')
+    this.setState({data:res.data})
+
+  }
+
+
   render() {
+    // const formated = date.format("dd.mm.yyyy");
     return (
         // <View></View>
         <View style={styles.container}>
-          <Card containerStyle={styles.card}>
-          </Card>
           <View style={styles.topContainer} />
           <View style={styles.bottomContainer} />
           <View style={styles.textContainer}>
-            <Text style={styles.topText}>Today</Text>
+            <Text style={styles.topText}>{this.state.markedDate}</Text>
+            <Text style={styles.topText1}>Today</Text>
           </View>
           <View style={styles.textContainer}>
             <Text style={styles.bottomText1}>Woah!</Text>
             <Text style={styles.bottomText2}>Perfect Day</Text>
+            {/*<Text style={styles.bottomText2}>{date}</Text>*/}
+
           </View>
           <View style={styles.image1}>
-            <Image source={require('./assets/cloud.png')} style={{width: 120, height: 120}} />
+            <Image source={require('../assets/cloud.png')} style={{width: 120, height: 120}} />
           </View>
           <View style={styles.textContainer}>
             <Text style={styles.bottomText3}>
-              Clear skies. Sunny to partly {"\n"}cloudy in the afternoon. {"\n"} Low 47 F
+              {this.state.data.weather.map(item=>(
+                  <Text>{item.description}</Text>
+              ))}.   {"\n"}{this.state.data.main.temp_min}
             </Text>
           </View>
           <View style={styles.image2}>
-            <Image source={require('./assets/temperature.png')} style={{width: 40, height: 40}} />
+            <Image source={require('../assets/temperature.png')} style={{width: 40, height: 40}} />
+            <Text style={{marginLeft:40, marginTop:-25}}>{this.state.data.main.temp} C</Text>
           </View>
           <View style={styles.image3}>
-            <Image source={require('./assets/temperature.png')} style={{width: 40, height: 40}} />
+            <Image source={require('../assets/pred.png')} style={{width: 40, height: 40}} />
+            <Text style={{marginLeft:40, marginTop:-25}}>{this.state.data.main.temp} %</Text>
           </View>
           <View style={styles.image4}>
-            <Image source={require('./assets/wind.png')} style={{width: 30, height: 30}} />
+            <Image source={require('../assets/wind.png')} style={{width: 30, height: 30}} />
+            <Text style={{marginLeft:40, marginTop:-25}}>{this.state.data.wind.speed} %</Text>
           </View>
           <View style={styles.image5}>
-            <Image source={require('./assets/humidity.png')} style={{width: 30, height: 30}} />
+            <Image source={require('../assets/humidity.png')} style={{width: 30, height: 30}} />
+            <Text style={{marginLeft:40, marginTop:-25}}>{this.state.data.main.humidity} Kmph</Text>
           </View>
           <View style={styles.textContainer}>
             <Text style={styles.bottomText4}>Temperature</Text>
-            <Text style={styles.bottomText5}>precipitation</Text>
+            <Text style={styles.bottomText5}>Precipitation</Text>
             <Text style={styles.bottomText6}>Wind</Text>
             <Text style={styles.bottomText7}>Humidity</Text>
           </View>
@@ -83,8 +112,18 @@ const styles = StyleSheet.create({
     // fontWeight: 'bold',
     fontSize: 20,
     fontFamily: 'serif',
-    marginTop: '40%',
-    marginLeft: '55%',
+    marginTop: 40,
+    marginLeft: 120,
+    // width: 200,
+    color: 'white',
+  },
+  topText1: {
+    textAlign: 'center', // <-- the magic
+    // fontWeight: 'bold',
+    fontSize: 20,
+    fontFamily: 'serif',
+    marginTop: 40,
+    marginLeft: 120,
     // width: 200,
     color: 'white',
   },
@@ -102,7 +141,7 @@ const styles = StyleSheet.create({
     // textAlign: 'center', // <-- the magic
     fontWeight: 'bold',
     fontFamily: 'serif',
-    fontSize: 30,
+    fontSize: 20,
     marginTop: '2%',
     marginLeft: '15%',
     // width: 200,
@@ -113,8 +152,8 @@ const styles = StyleSheet.create({
     // fontWeight: 'bold',
     fontSize: 20,
     fontFamily: 'serif',
-    marginTop: '70%',
-    marginLeft: '40%',
+    marginTop: '75%',
+    marginLeft: '45%',
     // width: 200,
     color: 'black',
   },
@@ -122,9 +161,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     // fontWeight: 'bold',
     fontFamily: 'serif',
-    fontSize: 20,
-    marginTop: '110%',
-    marginLeft: '-40%',
+    fontSize: 18,
+    marginTop: 400,
+    marginLeft: -150,
     // width: 200,
     color: 'black',
   },
@@ -132,9 +171,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     // fontWeight: 'bold',
     fontFamily: 'serif',
-    fontSize: 20,
-    marginTop: '-7%',
-    marginLeft: '60%',
+    fontSize: 18,
+    marginTop: -30,
+    marginLeft: 220,
     // width: 200,
     color: 'black',
   },
@@ -142,7 +181,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     // fontWeight: 'bold',
     fontFamily: 'serif',
-    fontSize: 20,
+    fontSize: 18,
     marginTop: '40%',
     marginLeft: '-60%',
     // width: 200,
@@ -152,7 +191,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     // fontWeight: 'bold',
     fontFamily: 'serif',
-    fontSize: 20,
+    fontSize: 18,
     marginTop: '-7%',
     marginLeft: '50%',
     // width: 200,
